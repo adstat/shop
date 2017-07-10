2ced1245b73c04e30ffe62be0059e4ad4ac9539d
xiangyiao <419646284@qq.com>
2017-07-10 14:20:37 +0800

    ceshi

--- invapi.php
+++ invapi.php
@@ -1,15 +1,14 @@
 <?php
 date_default_timezone_set('Asia/Shanghai');
 require_once 'config.php';
-
+var_dump(885454);
 //Load PHP XMLRPC
 require_once DIR_PATH.'/api/xmlrpc/v1/xmlrpc.php';
 require_once DIR_PATH.'/api/xmlrpc/v1/xmlrpcs.php';
 require_once DIR_PATH.'/api/xmlrpc/v1/xmlrpc_wrappers.php';
 
 
-
-
+var_dump(88899);
 
 define('APIURL', SITE_URI . '/api/xmlrpc/v1/index_warehouse.php');
 
